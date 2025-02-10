ec2579257f5ce5515b62639450f3dd1c85e5d913
Alestan Alves <48387196+alestanalves@users.noreply.github.com>
2025-02-10 10:47:52 -0300

    Update test_spi.py

--- test_spi.py
+++ test_spi.py
@@ -1,11 +1,7 @@
 import spidev
-
 spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.max_speed_hz = 8000000
-
-# Enviar e receber um byte
-response = spi.xfer2([0xAA])
+response = spi.xfer2([0xFF])
 print("Resposta SPI:", response)
-
 spi.close()
